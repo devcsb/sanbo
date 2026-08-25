0e0a400828efe9dee716f70a1da550e54f3e45ac
devcsb <devcsb119@gmail.com>
2026-08-25 22:20:20 +0900

    chore: prepare v0.8.0 release

--- lib/app/app_info.dart
+++ lib/app/app_info.dart
@@ -3,7 +3,7 @@ abstract final class AppInfo {
   static const nameKo = '산보';
   static const nameEn = 'sanbo';
   static const tagline = '작은 기록, 큰 흐름';
-  static const version = '0.7.2';
+  static const version = '0.8.0';
   static const versionLabel = '버전 $version';
   static const brandIconAsset = 'assets/branding/sanbo-icon.png';
   static const brandMainAsset = 'assets/branding/sanbo-main.jpg';
